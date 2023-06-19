65fc1621b501d313dad6d102416b19858fcb4e9e
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2023-06-20 00:17:18 +0200

    array-49/update

--- Array/49. Group Anagrams.py
+++ Array/49. Group Anagrams.py
@@ -4,12 +4,20 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashMap = defaultdict(list)
 
+        # T-> M * nlogN
+        # for x in strs:
+        #     s = sorted(x)
+        #     hashMap[tuple(s)].append(x)
+        #     #s = "".join(sorted(x))
+        #     #hashMap[s].append(x)
+
+        # T-> M * N
         for x in strs:
-            s = sorted(x)
-            hashMap[tuple(s)].append(x)
-            #s = "".join(sorted(x))
-            #hashMap[s].append(x)
-            
+            count = [0] * 26
+            for i in x:
+                count[ord(i) - ord('a')] += 1
+            hashMap[tuple(count)].append(x)
+    
         return hashMap.values()
 
 
